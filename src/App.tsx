c5f828b6a169c2a3b27d8f5e31243e3d9972b6bd
javar <javar@frasca.com>
2022-07-15 13:23:45 -0500

    add createWebSocket, Message

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import { useState } from "react"
+import React, {useRef, useState} from "react"
 import reactLogo from "./assets/react.svg"
 import "./App.css"
+import dayjs from "dayjs";
 
-type Global = { username: string | null }
+type Global = {
+    username: string | null
+}
 
 const global: Global = {
 	username: null
@@ -53,128 +56,134 @@ function ChatArea() {
 	)
 }
 
-function MessagingArea() {
+function MessagingArea(props: {$messaging_data: React.RefObject<HTMLTextAreaElement>}) {
 	return (
 		<div id="messaging_area">
 			<label>
-				<textarea id="messaging_data" maxLength={200} minLength={1} placeholder="Type a message" wrap="soft"></textarea>
+				<textarea ref={props.$messaging_data} id="messaging_data" maxLength={200} minLength={1} placeholder="Type a message" wrap="soft"></textarea>
 				<button id="messaging_button">Send</button>
 			</label>
 		</div>
 	)
 }
 
-const send_data = (socket: WebSocket) => (data = "") => {
-    const body = $messaging_data.value
-    $messaging_data.value = ""
 
-    const val = [data.trim(), body.trim()].find(e => e !== "")
-
-    if (val === undefined) return
-
-    socket.send(val)
-}
 
 /**
  * Spawns message in html
  */
- function MaterializeMessage(username: string, timestamp: string, body: string) {
-	let x = (
-		<>
-			<span className="message_username">{username}</span> @ <span className="message_timestamp">{timestamp}</span>
-		</>
+function Message(props: { username: string, timestamp: string, body: string }) {
+    if (props.body.match(new RegExp(`\/poke ${global.username}`))?.length === 1) {
+        audio["poke"].play()
+    } else if (props.username !== global.username) {
+        audio["message-received"].play()
+    }
+
+	return (
+        <div className="message">
+            <div className="message_head">
+                <span className="message_username">{props.username}</span> @ <span className="message_timestamp">{props.timestamp}</span>
+            </div>
+            <div className="message_body">
+                <p>{props.body}</p>
+            </div>
+        </div>
 	)
-	
-    let $message = document.createElement("div")
-    let $header = document.createElement("div")
-    // let $username = document.createElement("span")
-    let $timestamp = document.createElement("span")
-    let $body = document.createElement("div")
-    let $p = document.createElement("p")
-
-    // ------------------------------------------------------------------------
-
-    $message.classList.add("message")
-    $header.classList.add("message_head")
-    // $username.classList.add("message_username")
-    $timestamp.classList.add("message_timestamp")
-    $body.classList.add("message_body")
-
-    // ------------------------------------------------------------------------
-
-    $username.append(document.createTextNode(username))
-    $timestamp.append(document.createTextNode(timestamp))
-    $p.append(document.createTextNode(body))
-
-    $header.append($username, document.createTextNode(" @ "), $timestamp)
-    $body.append($p)
-    $message.append($header, $body)
-
-    $chat_area.append($message)
-
-    $message.scrollIntoView()
 }
 
-async function on_message(username, timestamp, body) {
-    MaterializeMessage(username, timestamp, body)
-	
-	if (body.match(new RegExp(`\/poke ${global.username}`))?.length === 1) {
-		await audio["poke"].play()
-	} else if (username !== global.username) {
-		await audio["message-received"].play()	
-	}
+function createWebSocket(setMessages: React.Dispatch<React.SetStateAction<JSX.Element[]>>) {
+    const events = {
+        onmessage: (event: MessageEvent<any>) => {
+            type WSMessageType = "message" | "user_joined" | "user_left"
+            type WSMessageDataDefault = { username: string, timestamp: string }
+            type WSMessageDataMessage = WSMessageDataDefault & { message: string }
+            type WSMessageDataUserJoined = WSMessageDataDefault
+            type WSMessageDataUserLeft = WSMessageDataDefault
+            type WSMessageData = WSMessageDataMessage & WSMessageDataUserJoined & WSMessageDataUserLeft
+
+            type WSMessage = { type: WSMessageType, data: WSMessageData }
+            const wsMessage: WSMessage = JSON.parse(event.data)
+
+            const events: Record<WSMessageType, (data: WSMessageData) => ReturnType<typeof Message>> = {
+                "message": (data: WSMessageDataMessage) =>
+                    <Message username={data.username} timestamp={data.timestamp} body={data.message}/>,
+                "user_joined": (data: WSMessageDataUserJoined) =>
+                    <Message username="[server]" timestamp={data.timestamp} body={`${data.username} has joined`}/>,
+                "user_left": (data: WSMessageDataUserLeft) =>
+                    <Message username="[server]" timestamp={data.timestamp} body={`${data.username} has left`}/>,
+            }
+
+            setMessages(messages => [...messages, events[wsMessage.type](wsMessage.data)])
+        },
+        onopen: (event: Event) => {
+            setMessages(messages => [
+                ...messages,
+                <Message username="[server]" timestamp={dayjs().format("hh:mma")} body="[open] Connection established"/>
+            ])
+        },
+        onclose: (event:  CloseEvent) => {
+            // if (event.wasClean) {
+            //     MaterializeMessage("[server]", event.data.timestamp, `[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`)
+            // } else {
+            //     // server process killed or network down, event.code is *usually* 1006 in this case
+            //     MaterializeMessage("[server]", event.data.timestamp, "[close] Connection died")
+            // }
+        },
+        onerror: (error: Event) => {
+            // MaterializeMessage("[server]", error.data.timestamp ?? dayjs().format("hh:mma"), `[error] ${error.message}`)
+        },
+    }
+
+    const socket = new WebSocket(`ws://${window.location.hostname}:8081/greeter?username=${global.username}`)
+    socket.onmessage = events.onmessage
+    socket.onopen = events.onopen
+    socket.onclose = events.onclose
+    socket.onerror = events.onerror
+    return socket
 }
 
 function App() {
-	global.username = window.prompt("what is your username") ?? "";
-	const socket = new WebSocket(`ws://${window.location.hostname}:8081/greeter?username=${global.username}`)
+    const bottomMostMessage = useRef()
 
-	type Message = { username: string, timestamp: string, message: string }
-	type Event = (data: Message) => void
-	socket.onmessage = event => {
-        const message = JSON.parse(event.data);
-        const events: Record<string, Event> = {
-            "message": (data: Message) => on_message(data.username, data.timestamp, data.message),
-            "user_joined": (data: Message) => on_message("[server]", data.timestamp, `${data.username} has joined`),
-            "user_left": (data: Message) => on_message("[server]", data.timestamp, `${data.username} has left`),
-        }
+    const [messages, setMessages] = useState([] as (ReturnType<typeof Message>)[])
 
-        events[message.type](message.data)
+    const send_data = (socket: WebSocket) => (data = "") => {
+        const body = $messaging_data.value
+        $messaging_data.value = ""
+
+        const val = [data.trim(), body.trim()].find(e => e !== "")
+
+        if (val === undefined) return
+
+        socket.send(val)
     }
 
-    socket.onopen = event => {
-        MaterializeMessage("[server]", dayjs().format("hh:mma"), "[open] Connection established")
-    };
+	global.username = window.prompt("what is your username") ?? "";
+    const socket = createWebSocket(setMessages)
 
-    socket.onclose = event => {
-        // if (event.wasClean) {
-        //     MaterializeMessage("[server]", event.data.timestamp, `[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`)
-        // } else {
-        //     // server process killed or network down, event.code is *usually* 1006 in this case
-        //     MaterializeMessage("[server]", event.data.timestamp, "[close] Connection died")
-        // }
-    };
+    const $messaging_data = useRef<HTMLTextAreaElement>(null)
 
-    socket.onerror = error => {
-        // MaterializeMessage("[server]", error.data.timestamp ?? dayjs().format("hh:mma"), `[error] ${error.message}`)
-    };
-	
-	const onKeyDown: React.KeyboardEventHandler<HTMLDivElement> = (e) => {
-        if (e.key === "Enter" && document.activeElement === $messaging_data) {
+	const onKeyDown: React.KeyboardEventHandler<HTMLDivElement> = (e: React.KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === "Enter" && document.activeElement === $messaging_data.current) {
             send_data(socket)()
         }
+
         if (e.key === "Escape") {
-            $messaging_data.blur()
+            $messaging_data.current?.blur()
         }
-        return true;
+
+        // what is this true about?
+        return true
 	}
-	
-	const [count, setCount] = useState(0)
 
 	return (
-		<div className="App" onKeyDown={onKeyDown}>
-			
-		</div>
+        <>
+            <button id="toggle_theme"></button>
+            <main id="chat" onKeyDown={onKeyDown}>
+                <div id="chat_area"></div>
+                <MessagingArea $messaging_data={$messaging_data}/>
+            </main>
+        </>
 	)
 }
 
