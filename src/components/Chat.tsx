674bc78512a59541c4afe12e712f1eac50f247f2
jaacko-torus <jaacko.torus@gmail.com>
2022-07-16 20:04:00 -0500

    prune comments

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -6,7 +6,42 @@ import dayjs from "dayjs"
 
 type M = ReturnType<typeof Message>
 
-export function createWebSocket(username: string, setMessages: React.Dispatch<React.SetStateAction<M[]>>) {
+function websocketCloseEventReasonHandler(code: number) {
+	// See https://www.rfc-editor.org/rfc/rfc6455#section-7.4.1
+	let reasonMapper: Record<number, string> = {
+		1000:
+			"Normal closure, meaning that the purpose for which the connection was established has been fulfilled.",
+		1001:
+			`An endpoint is "going away", such as a server going down or a browser having navigated away from a page.`,
+		1002:
+			"An endpoint is terminating the connection due to a protocol error",
+		1003:
+			"An endpoint is terminating the connection because it has received a type of data it cannot accept (e.g., an endpoint that understands only text data MAY send this if it receives a binary message).",
+		1004:
+			"Reserved. The specific meaning might be defined in the future.",
+		1005:
+			"No status code was actually present.",
+		1006:
+			"The connection was closed abnormally, e.g., without sending or receiving a Close control frame",
+		1007:
+			"An endpoint is terminating the connection because it has received data within a message that was not consistent with the type of the message (e.g., non-UTF-8 [https://www.rfc-editor.org/rfc/rfc3629] data within a text message).",
+		1008:
+			`An endpoint is terminating the connection because it has received a message that "violates its policy". This reason is given either if there is no other suitable reason, or if there is a need to hide specific details about the policy.`,
+		1009:
+			"An endpoint is terminating the connection because it has received a message that is too big for it to process.",
+		1010:
+			// Note that this status code is not used by the server, because it can fail the WebSocket handshake instead.
+			`An endpoint (client) is terminating the connection because it has expected the server to negotiate one or more extension, but the server didn't return them in the response message of the WebSocket handshake.\nSpecifically, the extensions that are needed are: ${code}.`,
+		1011:
+			"A server is terminating the connection because it encountered an unexpected condition that prevented it from fulfilling the request.",
+		1015:
+			"The connection was closed due to a failure to perform a TLS handshake (e.g., the server certificate can't be verified)."
+	}
+	
+	return (reasonMapper[code] ?? "Unknown reason.") + "\nFor more information, see https://www.rfc-editor.org/rfc/rfc6455#section-7.4.1."
+}
+
+function createWebsocket(username: string, setMessages: React.Dispatch<React.SetStateAction<M[]>>) {
 	//, messages: React.MutableRefObject<M[]>) {
 	//setMessages: React.Dispatch<React.SetStateAction<M[]>>) {
 		
@@ -42,30 +77,37 @@ export function createWebSocket(username: string, setMessages: React.Dispatch<Re
 				audio["message-received"].play()
 			}
 			
-			console.log("received?")
-			
-			//messages.current.push(events[wsMessage.type](wsMessage.data))
             setMessages(messages => [...messages, events[wsMessage.type](wsMessage.data)])
         },
         onopen: (event: Event) => {
-			// const ref = useRef<HTMLDivElement>(null)
-			
-			// messages.current.push(<Message username="[server]" timestamp={dayjs().format("hh:mma")} body="[open] Connection established"/>)
             setMessages(messages => [
                 ...messages,
 				<Message username="[server]" timestamp={dayjs().format("hh:mma")} body="[open] Connection established"/>
             ])
         },
         onclose: (event: CloseEvent) => {
-            // if (event.wasClean) {
-            //     MaterializeMessage("[server]", event.data.timestamp, `[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`)
-            // } else {
-            //     // server process killed or network down, event.code is *usually* 1006 in this case
-            //     MaterializeMessage("[server]", event.data.timestamp, "[close] Connection died")
-            // }
+			const reason = websocketCloseEventReasonHandler(event.code)
+			
+			setMessages(messages => [
+				...messages,
+				<Message
+					username="[server]"
+					timestamp={dayjs().format("hh:mma")}
+					body={[
+						`[close] Connection ${event.wasClean ? "closed cleanly" : "died"}`,
+						`\tcode=${event.code}`,
+						`\treason=${websocketCloseEventReasonHandler(event.code)}`
+					].join("\n")} />
+			])
         },
         onerror: (error: Event) => {
-            // MaterializeMessage("[server]", error.data.timestamp ?? dayjs().format("hh:mma"), `[error] ${error.message}`)
+			setMessages(messages => [
+				...messages,
+				<Message
+					username="[server]"
+					timestamp={dayjs().format("hh:mma")}
+					body={`[error]`}/>
+			])
         },
     }
 
@@ -81,19 +123,12 @@ export function createWebSocket(username: string, setMessages: React.Dispatch<Re
 export interface Args { username: string }
 const Chat = ({ username }: Args) => {
 	const [messages, setMessages] = useState<M[]>([])
-	const messagingDataRef = useRef<HTMLTextAreaElement>(null)
+	const [socket] = useState(() => createWebsocket(username, setMessages))
 	
-	const [socket] = useState(() => createWebSocket(username, setMessages))
-
-	const sendData = (data: string) => {
-		socket.send(data)
-		console.log("something happening?")
-	}
-
 	return (
 		<main id="chat">
 			<div id="chat_area">{messages}</div>
-			<MessagingArea sendData={sendData} messagingDataRef={messagingDataRef} />
+			<MessagingArea sendData={e => socket.send(e)} />
 		</main>
 	)
 }
--- src/components/MessagingArea.tsx
+++ src/components/MessagingArea.tsx
@@ -1,46 +1,45 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
-export interface Args { messagingDataRef: React.RefObject<HTMLTextAreaElement>, sendData: (data: string) => void }
-const MessagingArea = ({messagingDataRef, sendData}: Args) => {
+export interface Args { sendData: (data: string) => void }
+const MessagingArea = ({ sendData }: Args) => {
 	const [value, setValue] = useState("")
-	
+
+	const ref = useRef<HTMLTextAreaElement>(null)
+
 	const onKeyDown: React.KeyboardEventHandler<HTMLTextAreaElement> = (e) => {
-		if (e.key === "Enter") {
-			setValue(prev => prev.slice(0, Math.max(prev.length - 2, 0)))
+		if (e.key === "Enter" && document.activeElement === ref.current) {
+			e.preventDefault()
 			
-			if (document.activeElement === messagingDataRef.current) {
-				const trimmed_value = value.trim()
-				
-				if (trimmed_value.length !== 0) sendData(trimmed_value)
-				
-				setValue("")
-			}
+			const trimmed_value = value.trim()
+
+			if (trimmed_value.length !== 0) sendData(trimmed_value)
+
+			setValue(prev => "")
 		}
 
 		if (e.key === "Escape") {
-			messagingDataRef.current?.blur()
+			ref.current?.blur()
 		}
-
-		// what is this true about?
-		return true
 	}
-	
-	return (<div id="messaging_area">
-		<label>
-			<textarea
-				ref={messagingDataRef}
-				id="messaging_data"
-				minLength={1}
-				maxLength={200}
-				placeholder="Type a message"
-				wrap="soft"
-				value={value}
-				onKeyDown={onKeyDown}
-				onChange={e => setValue(e.target.value ?? "")}
-			></textarea>
-			<button id="messaging_button">Send</button>
-		</label>
-	</div>)
+
+	return (
+		<div id="messaging_area">
+			<label>
+				<textarea
+					ref={ref}
+					id="messaging_data"
+					minLength={1}
+					maxLength={200}
+					placeholder="Type a message"
+					wrap="soft"
+					value={value}
+					onKeyDown={onKeyDown}
+					onChange={e => setValue(e.target.value ?? "")}
+				></textarea>
+				<button id="messaging_button">Send</button>
+			</label>
+		</div>
+	)
 }
 
 export default MessagingArea
